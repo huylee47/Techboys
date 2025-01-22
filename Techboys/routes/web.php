3b785f8eb6074df4e59593e0145db4aaf98bcbd5
huylee47 <huyw300@gmail.com>
2025-01-22 16:06:38 +0700

    route

--- Techboys/routes/web.php
+++ Techboys/routes/web.php
@@ -1,11 +1,21 @@
 <?php
 
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('client.home.home');
 });
-Route::get('blog', function () {
-    return view('admin.tag.edit');
-}); 
 
+Route::get('login', function () {return view('admin.log.login');})->name('login.view');
+Route::post('/login/auth',[UserController::class,'login'])->name('login');
+
+Route::middleware(['auth'])->group(function () {
+    Route::prefix('admin')->group(function () {
+        Route::get('/home',[DashboardController::class,'index'])->name('admin.index');
+        Route::get('/blog', function () {
+        return view('admin.tag.edit');
+        });
+    });
+});
