50cf728addea1014897b5cda802e656ec28911a1
huylee47 <huyw300@gmail.com>
2025-01-22 16:06:28 +0700

    login view

--- Techboys/resources/views/admin/log/login.blade.php
+++ Techboys/resources/views/admin/log/login.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Tài Lộc | Đăng nhập </title>
+    <title>Techboys | Đăng nhập </title>
     <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@300;400;600;700;800&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="admin/assets/css/bootstrap.css">
     <link rel="stylesheet" href="admin/assets/vendors/bootstrap-icons/bootstrap-icons.css">
@@ -22,10 +22,10 @@
                     <h1 class="auth-title">Đăng nhập</h1>
                     {{-- <p class="auth-subtitle mb-5">Log in with your data that you entered during registration.</p> --}}
 
-                    <form action="" method="POST">
+                    <form action="{{route('login')}}" method="POST">
                         @csrf
                         <div class="form-group position-relative has-icon-left mb-4">
-                            <input type="text" class="form-control form-control-xl" name="email" placeholder="nhập E-mail">
+                            <input type="text" class="form-control form-control-xl" name="username" placeholder="nhập tài khoản">
                             <div class="form-control-icon">
                                 <i class="bi bi-person"></i>
                             </div>
