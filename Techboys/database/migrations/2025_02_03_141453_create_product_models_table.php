52f125e5bc43fbb2e4a4f087b7d58fabfc0279f8
huylee47 <huyw300@gmail.com>
2025-02-06 14:19:49 +0700

    seeder data & migration

--- Techboys/database/migrations/2025_02_03_141453_create_product_models_table.php
+++ Techboys/database/migrations/2025_02_03_141453_create_product_models_table.php
@@ -11,10 +11,9 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('product_categories', function (Blueprint $table) {
+        Schema::create('product_models', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->softDeletes();
             $table->timestamps();
         });
     }
@@ -24,6 +23,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('product_categories');
+        Schema::dropIfExists('product_models');
     }
 };
