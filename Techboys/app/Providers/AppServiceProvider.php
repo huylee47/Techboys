425f9c1bcb9879f6877764522ebca9fb6fad08c6
huylee47 <huyw300@gmail.com>
2025-02-17 09:40:35 +0700

    setup provider variable

--- Techboys/app/Providers/AppServiceProvider.php
+++ Techboys/app/Providers/AppServiceProvider.php
@@ -2,7 +2,11 @@
 
 namespace App\Providers;
 
+use App\Models\Config;
+use App\Models\Product;
+use App\Models\ProductCategory;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\View;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +23,14 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        $config = Config::first();
+        $categories = ProductCategory::get();
+        $hotproducts = Product::orderBy('purchases','desc')->take(16)->get();
+        $newProduct = Product::orderBy('created_at', 'desc')->take(20)->get();
+
+        View::share('config', $config);
+        View::share('categories', $categories);
+        View::share('newProduct', $newProduct);
+        View::share('hotproducts', $hotproducts);
     }
 }
