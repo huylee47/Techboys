fc905721ef4e1ac056882ff774c78b4e45e03d63
huylee47 <huyw300@gmail.com>
2025-02-21 10:35:56 +0700

    add crsf token meta

--- Techboys/resources/views/client/layouts/master.blade.php
+++ Techboys/resources/views/client/layouts/master.blade.php
@@ -2,6 +2,7 @@
 <html lang="en-US" itemscope="itemscope" itemtype="http://schema.org/WebPage">
 <head>
     <meta charset="UTF-8">
+    <meta name="csrf-token" content="{{ csrf_token() }}" />
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=no">
     <title>@yield('title')</title>
     <link rel="stylesheet" type="text/css" href="{{ url('') }}/home/assets/css/bootstrap.min.css" media="all" />
