649884d383a0dfc11b55d200f16a4ec0658c82b4
huylee47 <huyw300@gmail.com>
2025-03-05 09:47:22 +0700

    update view provider

--- Techboys/app/Providers/AppServiceProvider.php
+++ Techboys/app/Providers/AppServiceProvider.php
@@ -9,6 +9,7 @@ use App\Models\ProductCategory;
 use App\Service\VoucherService;
 use App\Service\CartPriceService;
 use App\Service\CartService;
+use App\Service\ProductService;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\View;
 
@@ -34,15 +35,17 @@ class AppServiceProvider extends ServiceProvider
         $config = Config::first();
         $categories = ProductCategory::get();
         $hotproducts = Product::orderBy('purchases','desc')->take(16)->get();
-        $newProduct = Product::orderBy('created_at', 'desc')->take(20)->get();
+        $newProduct = Product::orderBy('created_at', 'desc')->take(20)->get();  
         $loadBanner = Banner::all();
         $cartService = app(CartService::class);
         View::share('config', $config);
         View::share('categories', $categories);
-        View::share('newProduct', $newProduct);
         View::share('hotproducts', $hotproducts);
         View::share('loadBanner', $loadBanner);
         View::composer('*', function ($view) {
+            $productService = app(ProductService::class);
+            $newProduct = $productService->getNewProducts();
+            $view->with('newProduct', $newProduct);
             $cartService = app(CartService::class); 
             $cartCount = $cartService->countItems();
             $view->with([
