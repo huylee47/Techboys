cca480046447e909dbb6c83acba4ba32e33c68d7
minh <dinhminh0403@gmail.com>
2025-03-06 19:54:35 +0700

    UpdateViewComment

--- Techboys/resources/views/client/product/detail.blade.php
+++ Techboys/resources/views/client/product/detail.blade.php
@@ -228,11 +228,150 @@
                                                 <!-- .product-actions -->
                                             </div>
                                         </form>
-
-                                        <!-- .product-actions-wrapper -->
                                     </div>
+                                    </div>
+                                        <div class="tab-pane" id="tab-reviews" role="tabpanel">
+                                                <div class="techmarket-advanced-reviews" id="reviews">
+                                                    <div class="advanced-review row">
+                                                        <div class="advanced-review-rating">
+                                                            <h2 class="based-title">Đánh giá</h2>
+                                                            <div class="avg-rating">
+                                                                <span class="avg-rating-number">5.0</span>
+                                                                <div title="Rated 5.0 out of 5" class="star-rating">
+                                                                    <span style="width:100%"></span>
+                                                                </div>
+                                                            </div>
+                                                            <!-- /.avg-rating -->
+                                                            <div class="rating-histogram">
+                                                                <div class="rating-bar">
+                                                                    <div title="Rated 5 out of 5" class="star-rating">
+                                                                        <span style="width:100%"></span>
+                                                                    </div>
+                                                                    <div class="rating-count">1</div>
+                                                                    <div class="rating-percentage-bar">
+                                                                        <span class="rating-percentage" style="width:100%"></span>
+                                                                    </div>
+                                                                </div>
+                                                                <div class="rating-bar">
+                                                                    <div title="Rated 4 out of 5" class="star-rating">
+                                                                        <span style="width:80%"></span>
+                                                                    </div>
+                                                                    <div class="rating-count zero">0</div>
+                                                                    <div class="rating-percentage-bar">
+                                                                        <span class="rating-percentage" style="width:0%"></span>
+                                                                    </div>
+                                                                </div>
+                                                                <div class="rating-bar">
+                                                                    <div title="Rated 3 out of 5" class="star-rating">
+                                                                        <span style="width:60%"></span>
+                                                                    </div>
+                                                                    <div class="rating-count zero">0</div>
+                                                                    <div class="rating-percentage-bar">
+                                                                        <span class="rating-percentage" style="width:0%"></span>
+                                                                    </div>
+                                                                </div>
+                                                                <div class="rating-bar">
+                                                                    <div title="Rated 2 out of 5" class="star-rating">
+                                                                        <span style="width:40%"></span>
+                                                                    </div>
+                                                                    <div class="rating-count zero">0</div>
+                                                                    <div class="rating-percentage-bar">
+                                                                        <span class="rating-percentage" style="width:0%"></span>
+                                                                    </div>
+                                                                </div>
+                                                                <div class="rating-bar">
+                                                                    <div title="Rated 1 out of 5" class="star-rating">
+                                                                        <span style="width:20%"></span>
+                                                                    </div>
+                                                                    <div class="rating-count zero">0</div>
+                                                                    <div class="rating-percentage-bar">
+                                                                        <span class="rating-percentage" style="width:0%"></span>
+                                                                    </div>
+                                                                </div>
+                                                            </div>
+                                                            <!-- /.rating-histogram -->
+                                                        </div>
+                                                        <!-- /.advanced-review-rating -->
+                                                        <div class="advanced-review-comment">
+                                                            <div id="review_form_wrapper">
+                                                                <div id="review_form">
+                                                                    <div class="comment-respond" id="respond">
+                                                                        <h3 class="comment-reply-title" id="reply-title">Thêm bình luận</h3>
+                                                                        <form novalidate="" class="comment-form" id="commentform" method="post" action="#">
+                                                                            <div class="comment-form-rating">
+                                                                                <label>Đánh giá của bạn</label>
+                                                                                <p class="stars">
+                                                                                    <span><a href="#" class="star-1">1</a><a href="#" class="star-2">2</a><a href="#" class="star-3">3</a><a href="#" class="star-4">4</a><a href="#" class="star-5">5</a></span>
+                                                                                </p>
+                                                                            </div>
+                                                                            <p class="comment-form-comment">
+                                                                                <label for="comment">Bình luận của bạn</label>
+                                                                                <textarea aria-required="true" rows="8" cols="45" name="comment" id="comment"></textarea>
+                                                                            </p>
+                                                                            <p class="comment-form-author">
+                                                                                <label for="author">Tên
+                                                                                </label>
+                                                                                <input type="text" aria-required="true" size="30" value="" name="author" id="author">
+                                                                            </p>
+                                                                            <div class="col-md-6 mb-3">
+                                                                                <label for="images" class="form-label">Ảnh </label>
+                                                                                <input class="form-control" type="file" id="images" name="image"
+                                                                                    accept="image/*">
+                                                                                <div id="image-preview-container" class="mt-3"
+                                                                                    style="display: flex; gap: 10px; flex-wrap: wrap;"></div>
+                                                                            </div>
+                                                                            <p class="form-submit">
+                                                                                <input type="submit" value="Bình luận" class="submit" id="submit" name="submit">
+                                                                                <input type="hidden" id="comment_post_ID" value="185" name="comment_post_ID">
+                                                                                <input type="hidden" value="0" id="comment_parent" name="comment_parent">
+                                                                            </p>
+                                                                        </form>
+                                                                        <!-- /.comment-form -->
+                                                                    </div>
+                                                                    <!-- /.comment-respond -->
+                                                                </div>
+                                                                <!-- /#review_form -->
+                                                            </div>
+                                                            <!-- /#review_form_wrapper -->
+                                                        </div>
+                                                        <!-- /.advanced-review-comment -->
+                                                    </div>
+                                                    <!-- /.advanced-review -->
+                                                    <div id="comments">
+                                                        <ol class="commentlist">
+                                                            <li id="li-comment-83" class="comment byuser comment-author-admin bypostauthor even thread-even depth-1">
+                                                                <div class="comment_container" id="comment-83">
+                                                                    <div class="comment-text">
+                                                                        <div class="star-rating">
+                                                                            <span style="width:100%">Rated
+                                                                                <strong class="rating">5</strong> out of 5</span>
+                                                                        </div>
+                                                                        <p class="meta">
+                                                                            <strong itemprop="author" class="woocommerce-review__author">first last</strong>
+                                                                            <span class="woocommerce-review__dash">&ndash;</span>
+                                                                            <time datetime="2017-06-21T08:05:40+00:00" itemprop="datePublished" class="woocommerce-review__published-date">June 21, 2017</time>
+                                                                        </p>
+                                                                        <div class="description">
+                                                                            <p>Wow great product</p>
+                                                                        </div>
+                                                                        <!-- /.description -->
+                                                                    </div>
+                                                                    <!-- /.comment-text -->
+                                                                </div>
+                                                                <!-- /.comment_container -->
+                                                            </li>
+                                                            <!-- /.comment -->
+                                                        </ol>
+                                                        <!-- /.commentlist -->
+                                                    </div>
+                                                    <!-- /#comments -->
+                                                </div>
+                                                <!-- /.techmarket-advanced-reviews -->
+                                            </div>
+                                        <!-- .product-actions-wrapper -->
+                                    
                                     <!-- .entry-summary -->
-                                </div>
+                               
                                 <!-- .single-product-wrapper -->
                                 {{-- OTHER --}}
                                 <!-- .brands-carousel -->
@@ -248,7 +387,26 @@
             <!-- .col-full -->
         </div>
     </div>
+    <script>
+        document.getElementById('images').addEventListener('change', function (event) {
+            let previewContainer = document.getElementById('image-preview-container');
+            previewContainer.innerHTML = '';
 
+            Array.from(event.target.files).forEach(file => {
+                let reader = new FileReader();
+                reader.onload = function (e) {
+                    let img = document.createElement('img');
+                    img.src = e.target.result;
+                    img.style.width = '100px';
+                    img.style.height = '100px';
+                    img.style.objectFit = 'cover';
+                    img.style.borderRadius = '5px';
+                    previewContainer.appendChild(img);
+                };
+                reader.readAsDataURL(file);
+            });
+        });
+    </script>
     <script>
         document.querySelectorAll(".choice").forEach(choice => {
             choice.addEventListener("click", function() {
