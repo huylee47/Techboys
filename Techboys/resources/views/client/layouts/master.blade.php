97b78ce577b66ba5aafb3e9e1c34c4c68949fcbc
Vuong Vu <vuongvu10899@gmail.com>
2025-03-09 00:44:02 +0700

    mastar about

--- Techboys/resources/views/client/layouts/master.blade.php
+++ Techboys/resources/views/client/layouts/master.blade.php
@@ -158,7 +158,9 @@
                         <nav id="primary-navigation" class="primary-navigation" aria-label="Primary Navigation"
                             data-nav="flex-menu">
                             <ul id="menu-primary-menu" class="nav yamm">
-                                
+                                <li class="menu-item animate-dropdown">
+                                    <a title="about us" href="{{ route('about') }}">Về chúng tôi</a>
+                                </li>
                                 <li class="menu-item animate-dropdown">
                                     <a title="Headphones Sale" href="product-category.html">Liên hệ</a>
                                 </li>
