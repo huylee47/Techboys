fd370dcc0b9ba25a33b3e5581013a3f8c70923a2
tieu-tien-ho-do <tranvantai19052004@gmail.com>
2025-03-12 01:05:24 +0700

    Fix dasboard#1

--- Techboys/resources/views/admin/dashboard/index.blade.php
+++ Techboys/resources/views/admin/dashboard/index.blade.php
@@ -8,7 +8,7 @@
         </header>
 
         <div class="page-heading">
-            <h3>Profile Statistics</h3>
+            <h3>Tổng quan trang web</h3>
         </div>
         <div class="page-content">
             <section class="row">
@@ -24,7 +24,7 @@
                                             </div>
                                         </div>
                                         <div class="col-md-8">
-                                            <h6 class="text-muted font-semibold">Profile Views</h6>
+                                            <h6 class="text-muted font-semibold">Lượt xem</h6>
                                             <h6 class="font-extrabold mb-0">112.000</h6>
                                         </div>
                                     </div>
@@ -41,7 +41,7 @@
                                             </div>
                                         </div>
                                         <div class="col-md-8">
-                                            <h6 class="text-muted font-semibold">Followers</h6>
+                                            <h6 class="text-muted font-semibold">Khách hàng đã đăng ký</h6>
                                             <h6 class="font-extrabold mb-0">183.000</h6>
                                         </div>
                                     </div>
@@ -58,7 +58,7 @@
                                             </div>
                                         </div>
                                         <div class="col-md-8">
-                                            <h6 class="text-muted font-semibold">Following</h6>
+                                            <h6 class="text-muted font-semibold">Khách hàng chưa đăng ký</h6>
                                             <h6 class="font-extrabold mb-0">80.000</h6>
                                         </div>
                                     </div>
@@ -75,7 +75,7 @@
                                             </div>
                                         </div>
                                         <div class="col-md-8">
-                                            <h6 class="text-muted font-semibold">Saved Post</h6>
+                                            <h6 class="text-muted font-semibold">Khách hàng bỏ đăng ký</h6>
                                             <h6 class="font-extrabold mb-0">112</h6>
                                         </div>
                                     </div>
@@ -96,70 +96,8 @@
                         </div>
                     </div>
                     <div class="row">
-                        <div class="col-12 col-xl-4">
-                            <div class="card">
-                                <div class="card-header">
-                                    <h4>Profile Visit</h4>
-                                </div>
-                                <div class="card-body">
-                                    <div class="row">
-                                        <div class="col-6">
-                                            <div class="d-flex align-items-center">
-                                                <svg class="bi text-primary" width="32" height="32" fill="blue"
-                                                    style="width:10px">
-                                                    <use
-                                                        xlink:href="admin/assets/vendors/bootstrap-icons/bootstrap-icons.svg#circle-fill" />
-                                                </svg>
-                                                <h5 class="mb-0 ms-3">Europe</h5>
-                                            </div>
-                                        </div>
-                                        <div class="col-6">
-                                            <h5 class="mb-0">862</h5>
-                                        </div>
-                                        <div class="col-12">
-                                            <div id="chart-europe"></div>
-                                        </div>
-                                    </div>
-                                    <div class="row">
-                                        <div class="col-6">
-                                            <div class="d-flex align-items-center">
-                                                <svg class="bi text-success" width="32" height="32" fill="blue"
-                                                    style="width:10px">
-                                                    <use
-                                                        xlink:href="admin/assets/vendors/bootstrap-icons/bootstrap-icons.svg#circle-fill" />
-                                                </svg>
-                                                <h5 class="mb-0 ms-3">America</h5>
-                                            </div>
-                                        </div>
-                                        <div class="col-6">
-                                            <h5 class="mb-0">375</h5>
-                                        </div>
-                                        <div class="col-12">
-                                            <div id="chart-america"></div>
-                                        </div>
-                                    </div>
-                                    <div class="row">
-                                        <div class="col-6">
-                                            <div class="d-flex align-items-center">
-                                                <svg class="bi text-danger" width="32" height="32" fill="blue"
-                                                    style="width:10px">
-                                                    <use
-                                                        xlink:href="admin/assets/vendors/bootstrap-icons/bootstrap-icons.svg#circle-fill" />
-                                                </svg>
-                                                <h5 class="mb-0 ms-3">Indonesia</h5>
-                                            </div>
-                                        </div>
-                                        <div class="col-6">
-                                            <h5 class="mb-0">1025</h5>
-                                        </div>
-                                        <div class="col-12">
-                                            <div id="chart-indonesia"></div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-12 col-xl-8">
+
+                        <div class="col-12">
                             <div class="card">
                                 <div class="card-header">
                                     <h4>Latest Comments</h4>
