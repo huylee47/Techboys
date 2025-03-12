ff04caa4123ada432c46834acb8095deceeaecf3
huylee47 <huyw300@gmail.com>
2025-03-12 19:22:30 +0700

    del private channel

--- Techboys/routes/channels.php
+++ Techboys/routes/channels.php
@@ -7,14 +7,4 @@ use Illuminate\Support\Facades\Log;
 Broadcast::channel('App.Models.User.{id}', function ($user, $id) {
     return (int) $user->id === (int) $id;
 });
-Broadcast::channel('chat.{chatId}', function ($user = null, $chatId) {
-    Log::info('Channel request:', ['user' => $user, 'chatId' => $chatId, 'guest_id' => session()->get('guest_id')]);
-
-    if (Auth::check()) {
-        return ['id' => $user->id]; 
-    } elseif (session()->has('guest_id')) {
-        return ['guest_id' => session()->get('guest_id')];
-    }
-    return false;
-});
 
