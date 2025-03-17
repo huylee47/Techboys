8979efeb12ad3209a3af820a2a9cfcaebfbccd46
tieu-tien-ho-do <tranvantai19052004@gmail.com>
2025-03-17 21:09:59 +0700

    thêm track online cho middlewaremiddleware

--- Techboys/bootstrap/app.php
+++ Techboys/bootstrap/app.php
@@ -16,6 +16,7 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->alias([
             'auth.admin'=> CheckRole::class,
+            'track.online'=> \App\Http\Middleware\TrackUserOnline::class,
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
