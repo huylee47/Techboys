46b5ff009181018427a0af4e76763bfca74470a1
tieu-tien-ho-do <tranvantai19052004@gmail.com>
2025-03-17 21:10:58 +0700

    chỉnh sửa 1 chút cho phù hơp với datadata

--- Techboys/resources/views/admin/dashboard/index.blade.php
+++ Techboys/resources/views/admin/dashboard/index.blade.php
@@ -24,8 +24,8 @@
                                             </div>
                                         </div>
                                         <div class="col-md-8">
-                                            <h6 class="text-muted font-semibold">Lượt xem</h6>
-                                            <h6 class="font-extrabold mb-0">112.000</h6>
+                                            <h6 class="text-muted font-semibold">Người đang trực tuyến</h6>
+                                            <h6 class="font-extrabold mb-0" >{{ $onlineUsers }}</h6>
                                         </div>
                                     </div>
                                 </div>
