2443981ca85b5972f50b62c9c2e42a16c3360460
huylee47 <huyw300@gmail.com>
2025-03-17 16:52:53 +0700

    update bill ctrl

--- Techboys/app/Http/Controllers/BillController.php
+++ Techboys/app/Http/Controllers/BillController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\BillRequest;
 use App\Models\Bill;
+use App\Models\BillDetails;
+use App\Models\Promotion;
 use App\Models\User;
 use App\Models\Status;
 use App\Models\Voucher;
@@ -15,7 +17,7 @@ class BillController extends Controller
 {
     public function index()
     {
-        $bills = Bill::paginate(10);
+        $bills = Bill::orderBy('status_id')->paginate(10);
         return view('admin.bill.index', compact('bills'));
     }
 
@@ -37,12 +39,57 @@ class BillController extends Controller
 
     public function download($id)
     {
-        $bill = Bill::where('id', $id)->first();
+        $bill = Bill::findOrFail($id);
+        $billDetails = BillDetails::where('bill_id', $bill->id)->get();
+        $productPromotions = Promotion::get();
+        $total = $billDetails->sum(function ($detail) {
+            return $detail->quantity * $detail->price;
+        });
 
-        $pdf = Pdf::loadView('admin.bill.my_invoice', compact('bill'))->setPaper('a2')->setOption([
+        $pdf = Pdf::loadView('admin.bill.my_invoice', compact('bill','billDetails','productPromotions','total'))->setPaper('a4')->setOption([
             'tempDir' => public_path(),
             'chroot' => public_path(),
         ]);
-        return $pdf->download('invoice.pdf');
+        $fileName = 'bill_' . $bill->order_id . '_' . $bill->created_at->format('Ymd') . '.pdf';
+        return $pdf->download($fileName);
     }
+    public function invoiceBill($id) {
+        $bill = Bill::where('id', $id)->first();
+    
+        if (!$bill) {
+            return redirect()->route('admin.bill.index')->with('error', 'Không tìm thấy hóa đơn!');
+        }
+    
+        if ($bill->status_id != 1) {
+            return redirect()->route('admin.bill.index')->with('error', 'Hoá đơn không hợp lệ để xuất!');
+        }
+    
+        try {
+            $bill->update(['status_id' => 2]);
+            return redirect()->route('admin.bill.index')->with('success', 'Hoá đơn đã được xuất!');
+        } catch (\Exception $e) {
+            return redirect()->route('admin.bill.index')->with('error', 'Đã xảy ra lỗi khi xuất hoá đơn!');
+        }
+    }
+    
+    public function cancelBill($id) {
+        $bill = Bill::where('id', $id)->first();
+    
+        if (!$bill) {
+            return redirect()->route('admin.bill.index')->with('error', 'Không tìm thấy hóa đơn!');
+        }
+    
+        if ($bill->status_id != 1) {
+            return redirect()->route('admin.bill.index')->with('error', 'Hoá đơn không hợp lệ để xuất!');
+        }
+    
+        try {
+            $bill->update(['status_id' => 0]);
+            return redirect()->route('admin.bill.index')->with('success', 'Hoá đơn đã được huỷ!');
+        } catch (\Exception $e) {
+            return redirect()->route('admin.bill.index')->with('error', 'Đã xảy ra lỗi khi huỷ hoá đơn!');
+        }
+    }
+
+    
 }
