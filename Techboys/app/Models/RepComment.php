d202fd7c4163fd04230c92033c38a6b26aeeddf3
minh <dinhminh0403@gmail.com>
2025-03-18 20:05:17 +0700

    UpdateData

--- Techboys/app/Models/RepComment.php
+++ Techboys/app/Models/RepComment.php
@@ -7,9 +7,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class RepComment extends Model
 {
-    //
     use HasFactory;
-    protected $table = 'repcomment';
-    protected $fillable = ['user_id', 'product_id', 'content', 'rate','file_id','rep_content', 'comment_id'];
+    protected $table = 'rep_comment'; // Update table name
+    protected $fillable = ['user_id', 'product_id', 'content', 'rate', 'file_id', 'rep_content', 'comment_id'];
+
+    public function comment() {
+        return $this->belongsTo(Comment::class, 'comment_id');
+    }
 
 }
