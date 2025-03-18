98da5727ee5375cb4dcfa9c3521569e2b83f27f7
minh <dinhminh0403@gmail.com>
2025-03-18 20:05:28 +0700

    UpdateData

--- Techboys/app/Models/RepComment.php
+++ Techboys/app/Models/RepComment.php
@@ -15,4 +15,9 @@ class RepComment extends Model
         return $this->belongsTo(Comment::class, 'comment_id');
     }
 
+    public function user() {
+        return $this->belongsTo(User::class);
+    }
+
+ 
 }
