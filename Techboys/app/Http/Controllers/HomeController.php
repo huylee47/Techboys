c8d4591afbf3ec44ae380629d1c6bd1a6e9d1aad
Vuong Vu <vuongvu10899@gmail.com>
2025-03-18 21:56:03 +0700

    sửa controller

--- Techboys/app/Http/Controllers/HomeController.php
+++ Techboys/app/Http/Controllers/HomeController.php
@@ -2,9 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\ProductCategory;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
-    //
+    public function index()
+    {
+        $loadAll = ProductCategory::all();
+        return view('client.home.home', compact('loadAll'));
+    }
 }
