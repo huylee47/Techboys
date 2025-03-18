cffe299d9f2ab915261b05b5b661efcfabb7c201
minh <dinhminh0403@gmail.com>
2025-03-18 20:00:57 +0700

    UpdateData

--- Techboys/database/migrations/2025_03_17_094740_rep_comment.php
+++ Techboys/database/migrations/2025_03_17_094740_rep_comment.php
@@ -11,14 +11,13 @@ return new class extends Migration
      */
     public function up(): void
     {
-        //
         Schema::create('rep_comment', function (Blueprint $table) {
             $table->id();
             $table->foreignId('user_id')->nullable();
             $table->foreignId('comment_id');
             $table->foreignId('product_id');
-            $table->decimal('rate', 10, 2);
-            $table->text('content');
+            $table->decimal('rate', 10, 2)->default(0);
+            $table->text('content'); // Ensure content is not nullable
             $table->foreignId('file_id')->nullable();
             $table->text('rep_content')->nullable();
             $table->timestamps();
@@ -30,7 +29,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        //
         Schema::dropIfExists('rep_comment');
     }
 };
