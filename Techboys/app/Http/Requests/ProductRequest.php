f51e9e69c6ee9d94fa79f44107fa859a32781a87
huylee47 <huyw300@gmail.com>
2025-03-19 17:31:29 +0700

    update product request

--- Techboys/app/Http/Requests/ProductRequest.php
+++ Techboys/app/Http/Requests/ProductRequest.php
@@ -27,14 +27,6 @@ class ProductRequest extends FormRequest
             'category_id' => 'required',
             'description' => 'required',
             'img' => 'image|mimes:jpeg,png,jpg,gif|max:2048',
-            'color_id' => 'required|array',
-            'color_id.*' =>'required',
-            'model_id' => 'required|array',
-            'model_id.*' =>'required',
-            'price' => 'required|array',
-            'price.*' =>'required|min:0',
-            'stock' => 'required|array',
-            'stock.*' =>'required|min:0',
         ];
 
         return $rules;
@@ -51,12 +43,6 @@ class ProductRequest extends FormRequest
             'img.image' => 'Ảnh không hợp lệ.',
             'img.mimes' => 'Ảnh phải có định dạng jpeg, png, jpg, gif.',
             'img.max' => 'Kích thước ảnh tối đa là 2MB.',
-            'color_id.required' =>'Vui lòng chọn màu.',
-            'model_id.required' =>'Vui lòng chọn dung lượng máy.',
-            'price.*' => 'Vui lòng nhập giá.',
-            'price.*.min'=> 'Giá không được nhỏ hơn 0.',
-            'stock.*' => 'Vui lòng nhập số lượng.',
-            'stock.*.min'=> 'Số lượng không được nhỏ hơn 0.'
         ];
     }
 }
