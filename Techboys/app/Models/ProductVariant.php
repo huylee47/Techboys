ca832552e7172ef54c0a8a993c886795c0e837f9
Vuong Vu <vuongvu10899@gmail.com>
2025-03-20 14:53:05 +0700

    fix stock

--- Techboys/app/Models/ProductVariant.php
+++ Techboys/app/Models/ProductVariant.php
@@ -8,23 +8,39 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 
 class ProductVariant extends Model
 {
-    use HasFactory,SoftDeletes;
+    use HasFactory, SoftDeletes;
     protected $table = 'product_variants';
-    protected $fillable = ['product_id', 'color_id', 'price', 'stock','model_id'];
+    protected $fillable = ['product_id', 'color_id', 'price', 'stock', 'model_id'];
 
-    public function product(){
+    public function product()
+    {
         return $this->belongsTo(Product::class);
     }
-    public function billDetails(){
+    public function billDetails()
+    {
         return $this->hasMany(BillDetails::class);
-    }  
-    public function cart(){
+    }
+    public function cart()
+    {
         return $this->hasMany(Cart::class);
     }
-    public function color(){
+    public function color()
+    {
         return $this->belongsTo(Color::class);
     }
-    public function model(){
+    public function model()
+    {
         return $this->belongsTo(ProductModel::class);
+    }
+
+    public function getCalculatedStockAttribute()
+    {
+        $billedStock = $this->billDetails()
+            ->whereHas('bill', function ($query) {
+                $query->where('status_id', 1); 
+            })
+            ->sum('quantity');
+
+        return $this->stock + $billedStock; 
     }
 }
