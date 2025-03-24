dcf2b51a02d241ff78bb4fbe2f717698fd7b4c25
huylee47 <huyw300@gmail.com>
2025-03-24 10:15:21 +0700

    update master view

--- Techboys/resources/views/admin/layouts/master.blade.php
+++ Techboys/resources/views/admin/layouts/master.blade.php
@@ -153,6 +153,7 @@
 
     <script src="{{ url('') }}/admin/assets/js/main.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+
     <script src="https://cdn.jsdelivr.net/npm/bootstrap-select@1.14.0-beta2/dist/js/bootstrap-select.min.js"></script>
     <script src="{{ url('') }}/admin/assets/vendors/summernote/summernote-lite.min.js"></script>
     <script>
