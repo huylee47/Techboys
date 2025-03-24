42c008216e04c839edc394cbc74fc1a8c0e82f9a
huylee47 <huyw300@gmail.com>
2025-03-24 11:28:45 +0700

    update model

--- Techboys/database/migrations/2025_01_13_012955_create_bill_details_table.php
+++ Techboys/database/migrations/2025_01_13_012955_create_bill_details_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('bill_details', function (Blueprint $table) {
             $table->id();
             $table->foreignId('bill_id');
-            $table->foreignId('product_id');
+            $table->foreignId('product_id')->nullable();
             $table->foreignId('variant_id')->nullable();
             $table->integer('quantity');
             $table->decimal('price', 10, 2);
