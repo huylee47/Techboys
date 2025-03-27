f61afb3b2e18ccb43e3672ec7c13dc10884a8211
minh <dinhminh0403@gmail.com>
2025-03-27 21:15:08 +0700

    UpdateCtrl

--- Techboys/app/Http/Controllers/BillController.php
+++ Techboys/app/Http/Controllers/BillController.php
@@ -255,6 +255,13 @@ class BillController extends Controller
 
     public function submitCancelOrder(Request $request, $id)
     {
+        $request->validate([
+            'cancel_reason' => 'required|string|max:255',
+        ], [
+            'cancel_reason.required' => 'Lý do hủy đơn không được để trống.',
+            'cancel_reason.max' => 'Lý do hủy đơn không được vượt quá 255 ký tự.',
+        ]);
+
         $bill = Bill::find($id);
         try {
             DB::beginTransaction();
