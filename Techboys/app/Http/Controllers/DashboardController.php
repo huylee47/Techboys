64677e9ad68f4a27a2427b3b534d30ea254682af
tieu-tien-ho-do <tranvantai19052004@gmail.com>
2025-03-27 11:28:35 +0700

    sửa dashboard 11

--- Techboys/app/Http/Controllers/DashboardController.php
+++ Techboys/app/Http/Controllers/DashboardController.php
@@ -4,17 +4,31 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Cache;
-
+use App\Models\User;
+use App\Models\Comment;
+use App\Models\Product;
 class DashboardController extends Controller
 {
     public function index(){
-        $onlineUsers = Cache::get('online-users', []);
+    $latestComments = Comment::latest()->take(5)->get()->map(function ($comment) {
+        return [
+            'user' => User::find($comment->user_id),
+            'product' => Product::find($comment->product_id),
+            'content' => $comment->content
+        ];
+    });
+
+    $onlineUsers = Cache::get('online-users', []);
     $now = now();
     $onlineUsers = array_filter($onlineUsers, function ($expiresAt) use ($now) {
         return $expiresAt > $now;
     });
     Cache::put('online-users', $onlineUsers, now()->addMinutes(5));
 
-    return view('admin.dashboard.index', ['onlineUsers' => count($onlineUsers)]);
+    return view('admin.dashboard.index', [
+        'onlineUsers' => count($onlineUsers),
+        'registeredUsers' => User::where('role_id', 2)->count(),
+        'latestComments' => $latestComments
+    ]);
     }
 }
