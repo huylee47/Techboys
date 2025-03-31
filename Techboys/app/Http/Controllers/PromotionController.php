a4e75c378f752050ec6e390448f8998713a0aedd
tieu-tien-ho-do <tranvantai19052004@gmail.com>
2025-03-31 19:24:14 +0700

    promotion #1

--- Techboys/app/Http/Controllers/PromotionController.php
+++ Techboys/app/Http/Controllers/PromotionController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Promotion;
+use App\Models\Product;
 use App\Service\PromotionService;
 use Illuminate\Http\Request;
 
@@ -18,7 +19,8 @@ class PromotionController extends Controller
      */
     public function index()
     {
-        //
+        $promotions = Promotion::with('products')->latest()->take(20)->get();
+        return view('admin.promotions.index', compact('promotions'));
     }
 
     /**
@@ -26,7 +28,8 @@ class PromotionController extends Controller
      */
     public function create()
     {
-        //
+        $products = Product::all();
+        return view('admin.promotions.create', compact('products'));
     }
 
     /**
@@ -34,7 +37,18 @@ class PromotionController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $request->validate([
+            'name' => 'required|string|max:255',
+            'discount' => 'required|numeric|min:0|max:100',
+            'start_date' => 'required|date',
+            'end_date' => 'required|date|after:start_date',
+            'products' => 'required|array',
+        ]);
+
+        $promotion = Promotion::create($request->only(['name', 'discount', 'start_date', 'end_date']));
+        $promotion->products()->attach($request->products);
+
+        return redirect()->route('admin.promotions.index')->with('success', 'Khuyến mãi đã được tạo.');
     }
 
     /**
