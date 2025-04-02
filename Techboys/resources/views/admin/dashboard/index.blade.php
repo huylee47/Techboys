caa7c22350ac468d4fd82dd87a536d83e8746960
huylee47 <huyw300@gmail.com>
2025-04-02 13:31:12 +0700

    index view

--- Techboys/resources/views/admin/dashboard/index.blade.php
+++ Techboys/resources/views/admin/dashboard/index.blade.php
@@ -195,9 +195,9 @@
                         </div>
 
                     </div>
-                    {{-- <div class="card">
+                    <div class="card">
                         <div class="card-header">
-                            <h4>Recent Messages</h4>
+                            <h4>Hoá đơn được tạo gần đây</h4>
                         </div>
                         <div class="card-content pb-4">
                             <div class="recent-message d-flex px-4 py-3">
@@ -209,30 +209,11 @@
                                     <h6 class="text-muted mb-0">@johnducky</h6>
                                 </div>
                             </div>
-                            <div class="recent-message d-flex px-4 py-3">
-                                <div class="avatar avatar-lg">
-                                    <img src="">
-                                </div>
-                                <div class="name ms-4">
-                                    <h5 class="mb-1">Dean Winchester</h5>
-                                    <h6 class="text-muted mb-0">@imdean</h6>
-                                </div>
-                            </div>
-                            <div class="recent-message d-flex px-4 py-3">
-                                <div class="avatar avatar-lg">
-                                    <img src="">
-                                </div>
-                                <div class="name ms-4">
-                                    <h5 class="mb-1">John Dodol</h5>
-                                    <h6 class="text-muted mb-0">@dodoljohn</h6>
-                                </div>
-                            </div>
                             <div class="px-4">
-                                <button class='btn btn-block btn-xl btn-light-primary font-bold mt-3'>Start
-                                    Conversation</button>
+                                <button class='btn btn-block btn-xl btn-light-primary font-bold mt-3'>Quản lý đơn hàng</button>
                             </div>
                         </div>
-                    </div> --}}
+                    </div>
                     {{-- <div class="card">
                         <div class="card-header">
                             <h4>Visitors Profile</h4>
@@ -245,3 +226,8 @@
             </section>
         </div>
     @endsection
+    @section('scripts')
+    <script src="https://js.pusher.com/8.4.0/pusher.min.js"></script>
+    @vite(['resources/js/app.js'])
+    @endsection
+
