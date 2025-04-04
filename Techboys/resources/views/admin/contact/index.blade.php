9dad8d4b7c372ad8e2928e81506f122d17b41c09
tieu-tien-ho-do <tranvantai19052004@gmail.com>
2025-04-04 15:49:19 +0700

    contact#3 fix index balde bên admin

--- Techboys/resources/views/admin/contact/index.blade.php
+++ Techboys/resources/views/admin/contact/index.blade.php
@@ -16,7 +16,7 @@
                 <div class="col-12 col-md-6 order-md-2 order-first">
                     <nav aria-label="breadcrumb" class="breadcrumb-header float-start float-lg-end">
                         <ol class="breadcrumb">
-                            <li class="breadcrumb-item"><a href="index.html">Dashboard</a></li>
+                            <li class="breadcrumb-item"><a href="{{ route('admin.index') }}">Dashboard</a></li>
                             <li class="breadcrumb-item active" aria-current="page">Danh sách liên hệ</li>
                         </ol>
                     </nav>
@@ -35,36 +35,38 @@
                             {{ session('error') }}
                         </div>
                     @endif
-                    {{-- <a href="{{ route('admin.contact.show_add') }}" class="btn btn-primary">Thêm liên hệ</a> --}}
                     <table class="table table-striped table-bordered" id="table1">
                         <thead>
                             <tr>
-                                <th class="col-2">Người liên hệ</th>
-                                <th class="col-2">Địa chỉ Email</th>
+                                <th class="col-3">Người liên hệ</th>
                                 <th class="col-2">Số điện thoại</th>
-                                <th class="col-4">Tin nhắn</th>
+                                <th class="col-3">Tin nhắn</th>
+                                <th class="col-2">Ngày tạo phản hồi</th>
                                 <th class="col-2">Hành động</th>
                             </tr>
                         </thead>
                         <tbody>
-
-                            <tr>
-                                <td>aaaaaaaqaaaaaaaq</td>
-                                <td>aaaaaaaqaaaaaaaq</td>
-                                <td>aaaaaaaqaaaaaaaq</td>
-                                <td>aaaaaaaqaaaaaaaq</td>
-                                <td class="text-center">
-                                    <a href="" class="btn btn-info">Ẩn</a>
-                                    <a href="" class="btn btn-warning">Sửa</a>
-                                </td>
-                            </tr>
-
-
+                            @foreach ($contacts as $contact)
+                                <tr>
+                                    <td>{{ $contact->name }}</td>
+                                    <td>{{ $contact->phone }}</td>
+                                    <td>{{ Str::limit($contact->message, 50) }}</td>
+                                    <td>{{ $contact->created_at->format('d/m/Y H:i') }}</td>
+                                    <td class="text-center">
+                                        {{-- <a href="{{ route('admin.contact.show', $contact->id) }}" class="btn btn-info mb-1">Xem chi tiết</a> --}}
+                                        <form action="{{ route('admin.contact.destroy', $contact->id) }}" method="POST" onsubmit="return confirm('Bạn có chắc chắn muốn xoá không?')" style="display:inline-block">
+                                            @csrf
+                                            @method('DELETE')
+                                            <button type="submit" class="btn btn-danger">Xoá</button>
+                                        </form>
+                                    </td>
+                                </tr>
+                            @endforeach
                         </tbody>
                     </table>
                 </div>
             </div>
-
         </section>
     </div>
-    @endsection
+</div>
+@endsection
