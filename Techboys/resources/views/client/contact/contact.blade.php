b99e5b6982aeeaecf66f0829c9cdde9cf96c404c
tieu-tien-ho-do <tranvantai19052004@gmail.com>
2025-04-04 18:52:59 +0700

    lấy map từ config

--- Techboys/resources/views/client/contact/contact.blade.php
+++ Techboys/resources/views/client/contact/contact.blade.php
@@ -16,7 +16,7 @@
                     <div class="type-page hentry">
                         <div class="entry-content">
                             <div class="stretch-full-width-map">
-                                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d634.1549045120821!2d106.70951524863375!3d20.865679643682185!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x314a7bf67d69c9c7%3A0x4341c6cef1813f18!2zVHLGsOG7nW5nIENhbyDEkeG6s25nIEZQVCBQb2x5dGVjaG5pYw!5e0!3m2!1svi!2s!4v1740505370576!5m2!1svi!2s" height="514" style="border:0;" allowfullscreen="" ></iframe>
+                                {!! $config->map !!}
                             </div>
                             <div class="row contact-info">
                                 <div class="col-md-9 left-col">
