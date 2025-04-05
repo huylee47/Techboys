d099356a70a15fd693b6a1caf44745d8345451ee
huylee47 <huyw300@gmail.com>
2025-04-05 16:59:12 +0700

    master view

--- Techboys/resources/views/admin/layouts/master.blade.php
+++ Techboys/resources/views/admin/layouts/master.blade.php
@@ -210,6 +210,9 @@
                 {{-- Modal ChatBot --}}
                 <div id="bot-chat-icon" class="bot-real-time-icon">
                     <img src="{{ url('') }}/admin/assets/images/config/{{ $config->favicon }}" alt="" width="40px">
+                    <div id="chat-title" class="chat-title">
+                        Cần trợ giúp, chat cùng Techboys AI ngay
+                    </div>
                 </div>
             
                 <div id="bot-chat-modal" class="bot-real-time-box">
