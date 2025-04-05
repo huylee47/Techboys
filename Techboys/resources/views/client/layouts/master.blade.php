f98ae808b93c8b24fbbf77c2eda7f7b45e388715
huylee47 <huyw300@gmail.com>
2025-04-05 16:50:08 +0700

    update master view client

--- Techboys/resources/views/client/layouts/master.blade.php
+++ Techboys/resources/views/client/layouts/master.blade.php
@@ -29,6 +29,7 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css">
     <link rel="stylesheet" type="text/css" href="{{ url('') }}/home/assets/css/colors/blue.css" media="all" />
     <link href="https://fonts.googleapis.com/css?family=Rubik:300,400,500,900" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="{{ url('') }}/home/assets/css/chat-bot.css" media="all" />
     <link rel="shortcut icon" href="{{ url('') }}/admin/assets/images/config/{{ $config->favicon }}">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
     <link rel="shortcut icon" href="{{ url('') }}/admin/assets/images/config/{{ $config->favicon }}">
@@ -391,6 +392,23 @@
             </div>
 
         </div>
+        {{-- Modal ChatBot --}}
+        <div id="bot-chat-icon" class="bot-real-time-icon">
+            <img src="{{ url('') }}/admin/assets/images/config/{{ $config->favicon }}" alt="" width="40px">
+        </div>
+    
+        <div id="bot-chat-modal" class="bot-real-time-box">
+            <div class="bot-real-time-title">
+                <span>Chat với Techboys AI</span>
+                <span id="bot-close-chat" class="bot-real-time-close">&times;</span>
+            </div>
+            <div id="bot-chat-messages" class="bot-real-time-content">
+            </div>
+            <div class="bot-real-time-sent-content">
+                <input type="text" id="bot-chat-input" placeholder="Nhập tin nhắn..." onkeydown="if(event.key === 'Enter') sendMessage()" />
+                <button id="bot-send-message" onclick="sendMessage()">Gửi</button>
+            </div>
+        </div>
         <!-- #content -->
         <footer class="site-footer footer-v1">
             <div class="col-full">
@@ -591,7 +609,38 @@
                 });
         });
     </script>
+    <script>
+        $('#bot-chat-icon').click(function() {
+            $('#bot-chat-modal').toggle();
+            $(this).hide();
+        });
 
+        $('#bot-close-chat').click(function() {
+            $('#bot-chat-modal').hide();
+            $('#bot-chat-icon').show();
+        });
+
+        function sendMessage() {
+            var prompt = $('#bot-chat-input').val();
+            var chatMessages = $('#bot-chat-messages');
+            var sendButton = $('#bot-send-message');
+
+            if (prompt.trim() === '') return;
+
+            sendButton.prop('disabled', true);
+
+            chatMessages.append('<div class="bot-user-message">' + prompt + '</div>');
+
+            $.post('/ask-gemini', { _token: '{{ csrf_token() }}', prompt: prompt }, function(data) {
+                chatMessages.append('<div class="bot-bot-message">' + data.response + '</div>');
+                chatMessages.scrollTop(chatMessages[0].scrollHeight);
+                console.log(data.response);
+                sendButton.prop('disabled', false);
+            });
+
+            $('#bot-chat-input').val('');
+        }
+    </script>
 
 
 
