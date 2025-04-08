0177805b48fa9eff926f609ac65606c63d9e484d
minh <dinhminh0403@gmail.com>
2025-04-08 20:08:41 +0700

    UpdateView

--- Techboys/resources/views/client/blog/blog.blade.php
+++ Techboys/resources/views/client/blog/blog.blade.php
@@ -433,20 +433,7 @@
                                     <!-- .content-body -->
                                 </article>
                             @endforeach
-                            <nav class="navigation pagination" id="post-navigation">
-                                <span class="screen-reader-text">Posts navigation</span>
-                                <div class="nav-links">
-                                    <ul class="page-numbers">
-                                        <li><a href="#" class="page-numbers current">1</a></li>
-                                        <li>
-                                            <span class="page-numbers">2</span>
-                                        </li>
-                                        <li><a href="#" class="next page-numbers">Next</a></li>
-                                    </ul>
-                                    <!-- .page-numbers -->
-                                </div>
-                                <!-- .nav-links -->
-                            </nav>
+                          
 
                             <!-- .navigation -->
                         </main>
