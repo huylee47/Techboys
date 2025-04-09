80b75f02e8af6920c5d9a78bb10017e68db4310c
minh <dinhminh0403@gmail.com>
2025-04-09 13:38:54 +0700

    UpdateView

--- Techboys/resources/views/client/login/index.blade.php
+++ Techboys/resources/views/client/login/index.blade.php
@@ -1,27 +1,35 @@
 @extends('client.layouts.master')
 
 <style>
-  <style>
-        .alert-success {
-            color: #155724;
-            background-color: #d4edda;
-            border-color: #c3e6cb;
-            padding: 15px;
-            margin-bottom: 20px;
-            border: 1px solid transparent;
-            border-radius: 4px;
-        }
-        .alert-danger {
-            color: #721c24;
-            background-color: #f8d7da;
-            border-color: #f5c6cb;
-            padding: 15px;
-            margin-bottom: 20px;
-            border: 1px solid transparent;
-            border-radius: 4px;
-        }
-    </style>
+    .alert-success {
+        color: #155724;
+        background-color: #d4edda;
+        border-color: #c3e6cb;
+        padding: 15px;
+        margin-bottom: 20px;
+        border: 1px solid transparent;
+        border-radius: 4px;
+    }
+    .alert-danger {
+        color: #721c24;
+        background-color: #f8d7da;
+        border-color: #f5c6cb;
+        padding: 15px;
+        margin-bottom: 20px;
+        border: 1px solid transparent;
+        border-radius: 4px;
+    }
+    .form-control-icon {
+        position: absolute;
+        top: 50%;
+        transform: translateY(-50%);
+        right: 15px;
+    }
+    .form-group {
+        position: relative;
+    }
 </style>
+
 @section('main')
 <div class="container">
   <h1 >Đăng nhập</h1>
@@ -57,10 +65,7 @@
       <i class="bi bi-shield-lock"></i>
   </div>
 </div>
-  <div class="col-md-6 mb-3 form-check">
-    <input type="checkbox" class="form-check-input" id="exampleCheck1">
-    <label class="form-check-label" for="exampleCheck1">Ghi nhớ tôi</label>
-  </div>
+ 
   <button type="submit" class="btn btn-primary">Đăng nhập</button>
   <div>
     <p class="text-gray-600">Bạn chưa có tài khoản? <a href="{{ route('client.log.create') }}"
