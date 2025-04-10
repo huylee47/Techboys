309bb2de598bf2ae7c4c4d1de8c30318c2bdb61a
huylee47 <huyw300@gmail.com>
2025-04-10 12:08:48 +0700

    view text brand

--- Techboys/resources/views/admin/brand/index.blade.php
+++ Techboys/resources/views/admin/brand/index.blade.php
@@ -16,8 +16,8 @@
                     <div class="col-12 col-md-6 order-md-2 order-first">
                         <nav aria-label="breadcrumb" class="breadcrumb-header float-start float-lg-end">
                             <ol class="breadcrumb">
-                                <li class="breadcrumb-item"><a href="index.html">Dashboard</a></li>
-                                <li class="breadcrumb-item active" aria-current="page">Danh Mục</li>
+                                <li class="breadcrumb-item"><a href="{{route('admin.index')}}">Dashboard</a></li>
+                                <li class="breadcrumb-item active" aria-current="page">Thương hiệu</li>
                             </ol>
                         </nav>
                     </div>
@@ -37,7 +37,7 @@
                             </div>
                         @endif
 
-                        <a href="{{ route('admin.brand.create') }}" class="btn btn-primary">Thêm Danh mục</a>
+                        <a href="{{ route('admin.brand.create') }}" class="btn btn-primary">Thêm thương hiệu</a>
 
                         <table class="table table-striped table-bordered" id="table1">
                             <thead>
