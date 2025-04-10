9babd3fd7dc715a5c1e090a8c4fd8a6f25e7c13c
huylee47 <huyw300@gmail.com>
2025-04-10 14:55:25 +0700

    fix

--- Techboys/app/Http/Requests/BrandRequest.php
+++ Techboys/app/Http/Requests/BrandRequest.php
@@ -22,7 +22,8 @@ class BrandRequest extends FormRequest
     public function rules(): array
     {
         return [
-            'name' => 'required|string|max:255|unique:brands,name' .$this->id,
+            'name' => 'required|string|max:255|unique:brands,name,' . $this->id,
+
         ];
     }
     public function messages(): array
