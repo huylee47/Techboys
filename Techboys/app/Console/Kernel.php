521dca81a64d2cf49b83609e826ea67aaec9c3de
Vuong Vu <vuongvu10899@gmail.com>
2025-04-14 22:43:57 +0700

    sửa kernel

--- Techboys/app/Console/Kernel.php
+++ Techboys/app/Console/Kernel.php
@@ -30,6 +30,9 @@ class Kernel extends ConsoleKernel
         // Thêm dòng này: Chạy command 'bills:autoconfirm' hàng ngày
         $schedule->command('bills:autoconfirm')->daily();
 
+        // Đổi thành (để chạy mỗi phút):
+        $schedule->command('bills:autoconfirm')->everyMinute();
+
         // Hoặc chạy vào một giờ cụ thể hàng ngày (ví dụ: 1 giờ sáng)
         // $schedule->command('bills:autoconfirm')->dailyAt('01:00');
     }
