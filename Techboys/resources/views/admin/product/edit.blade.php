769b70e10f83024236711e00261ff27a409c2d14
huylee47 <huyw300@gmail.com>
2025-04-16 14:17:11 +0700

    update text

--- Techboys/resources/views/admin/product/edit.blade.php
+++ Techboys/resources/views/admin/product/edit.blade.php
@@ -230,11 +230,11 @@
             <div class="modal-dialog">
                 <div class="modal-content">
                     <div class="modal-header">
-                        <h5 class="modal-title" id="deleteModalLabel">Xác nhận xóa sản phẩm</h5>
+                        <h5 class="modal-title" id="deleteModalLabel"><span class="bi-exclamation-triangle-fill text-danger"> Xác nhận xóa</span></h5>
                         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                     </div>
                     <div class="modal-body">
-                        Bạn có chắc chắn muốn xóa sản phẩm <strong id="productName"></strong> không?
+                        Bạn có chắc chắn muốn  <span class="text-danger">xóa</span>  sản phẩm <strong id="productName"></strong> không?
                     </div>
                     <div class="modal-footer">
                         <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Hủy</button>
