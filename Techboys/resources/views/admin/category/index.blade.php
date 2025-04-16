2fbe2e6bba62e5e7240636bb62ddfa403a4599f4
huylee47 <huyw300@gmail.com>
2025-04-16 14:17:09 +0700

    update text

--- Techboys/resources/views/admin/category/index.blade.php
+++ Techboys/resources/views/admin/category/index.blade.php
@@ -86,11 +86,11 @@
             <div class="modal-dialog">
                 <div class="modal-content">
                     <div class="modal-header">
-                        <h5 class="modal-title" id="deleteModalLabel">Xác nhận xóa</h5>
+                        <h5 class="modal-title" id="deleteModalLabel"><span class="bi-exclamation-triangle-fill text-danger"> Xác nhận xóa</span></h5>
                         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                     </div>
                     <div class="modal-body">
-                        Bạn có chắc chắn muốn xóa danh mục <strong id="categoryName"></strong> không?
+                        Bạn có chắc chắn muốn <span class="text-danger">xóa</span> danh mục <strong id="categoryName"></strong> không?
                     </div>
                     <div class="modal-footer">
                         <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Hủy</button>
