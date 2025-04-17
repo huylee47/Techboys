089ce98b10ed76a8fe2d0b009c62497ef382fc4e
minh <dinhminh0403@gmail.com>
2025-04-17 15:20:59 +0700

    UpdateView

--- Techboys/resources/views/client/order/order.blade.php
+++ Techboys/resources/views/client/order/order.blade.php
@@ -46,7 +46,6 @@
                                                         <th>Mã đơn hàng</th>
                                                         @endauth
                                                         <th>Sản phẩm</th>
-                                                        <th>Số lượng</th>
                                                         <th>Trạng thái</th>
                                                         <th>Tổng cộng</th>
                                                         <th>Hành động</th>
@@ -61,7 +60,7 @@
                                                             <td>
                                                                 @foreach ($searchedOrder->billDetails as $detail)
                                                                     <div style="display: flex; align-items: center; gap: 10px;">
-                                                                        <img src="{{ url('') }}/admin/assets/images/product/{{ $detail->product->img }}" alt="{{ $detail->product->name }}" style="width: 50px; height: 50px; object-fit: cover;">
+                                                                        <img src="{{ url('') }}/admin/assets/images/product/{{ $detail->product->img }}" alt="{{ $detail->product->name }}" style="width: 120px; height: 120px; object-fit: cover;">
                                                                         {{ $detail->product->name }}
                                                                         @if($detail->variant_id)
                                                                             ({{ $detail->attributes }})
@@ -72,7 +71,8 @@
                                                             </td>
                                                             <td>
                                                                 @foreach ($searchedOrder->billDetails as $detail)
-                                                                    {{ $detail->quantity }}<br>
+                                                                <span style="margin-left: auto;">x{{ $detail->quantity }}</span>
+                                                                <br>
                                                                 @endforeach
                                                             </td>
                                                             <td>
@@ -135,7 +135,7 @@
                                                     <tr>
                                                         <th>Mã đơn hàng</th>
                                                         <th>Sản phẩm</th>
-                                                        <th>Số lượng</th>
+                                                 
                                                         {{-- <th>PT thanh toán</th>
                                                         <th>TT thanh toán</th> --}}
                                                         <th>Trạng thái</th>
@@ -157,15 +157,12 @@
                                                                         @if($detail->variant_id)
                                                                             ({{ $detail->attributes }})
                                                                         @endif
+                                                                        <span style="margin-left: auto;">x{{ $detail->quantity }}</span>
                                                                     </div>
                                                                     <br>
                                                                 @endforeach
                                                             </td>
-                                                            <td>
-                                                                @foreach ($bill->billDetails as $detail)
-                                                                    {{ $detail->quantity }}<br>
-                                                                @endforeach
-                                                            </td>
+                                                          
 
                                                             <td>
                                                                 @switch($bill->status_id)
