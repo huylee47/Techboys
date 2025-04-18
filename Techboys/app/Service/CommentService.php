c1e71b36635aa296f96f17a442fbd999ffa4a1ef
huylee47 <huyw300@gmail.com>
2025-04-18 09:25:56 +0700

    update cmt service

--- Techboys/app/Service/CommentService.php
+++ Techboys/app/Service/CommentService.php
@@ -3,6 +3,7 @@
 namespace App\Service;
 
 use App\Models\Comment;
+use App\Models\Product;
 use App\Models\RepComment;
 
 class CommentService
@@ -13,9 +14,15 @@ class CommentService
         $comment->user_id = $data['user_id'];
         $comment->product_id = $data['product_id'];
         $comment->content = $data['content'];
-        $comment->rate = $data['rate']; 
+        $comment->rate = $data['rate'];
         $comment->file_id = $data['file_id'] ?? null;
         $comment->save();
+
+        $averageRate = Comment::where('product_id', $data['product_id'])->avg('rate');
+
+        Product::where('id', $data['product_id'])->update([
+            'rate_average' => round($averageRate, 1)
+        ]);
 
         return $comment;
     }
@@ -27,9 +34,9 @@ class CommentService
         $reply->product_id = $data['product_id'];
         $reply->comment_id = $data['comment_id'];
         $reply->rep_content = $data['rep_content'];
-        $reply->content = $data['content']; 
+        $reply->content = $data['content'];
         $reply->rate = $data['rate'];
-        $reply->file_id = $data['file_id'] ?? null; 
+        $reply->file_id = $data['file_id'] ?? null;
         $reply->save();
 
         return $reply;
