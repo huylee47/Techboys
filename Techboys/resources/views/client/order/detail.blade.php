ed5bf9c73c7430f1dde951f7dd42efcd272e3f75
minh <dinhminh0403@gmail.com>
2025-04-30 21:37:16 +0700

    UpdateView

--- Techboys/resources/views/client/order/detail.blade.php
+++ Techboys/resources/views/client/order/detail.blade.php
@@ -53,14 +53,14 @@
                 4 => 'Giao hàng thành công',
                 5 => 'Yêu cầu hoàn hàng đang được',
                 6 => 'Đã xét duyệt hoàn hàng',
-                7 => 'Đang hoàn hàng',
-                8 => 'Hoàn hàng thành công',
+                7 => 'Hoàn hàng thành công',
+                8 => 'Hoàn hàng thất bại',
                 default => 'Không xác định',
             };
 
             $paymentText = match ($order->payment_method) {
-                1 => 'Chuyển khoản',
-                2 => 'Tiền mặt',
+                1 => 'VNPAY',
+                2 => 'Ship COD',
                 default => 'Không xác định',
             };
         @endphp
